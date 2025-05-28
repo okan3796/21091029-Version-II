0923a01eec4b1c19ffc704db74c46f66fb32afff
okan3796 <okanyilmaz37@gmail.com>
2025-05-28 15:21:15 +0300

    Uygulama tasarımını ve ilk ekranı tamamladım

--- App.js
+++ App.js
@@ -1,20 +1,25 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+
+import HomeScreen from './src/screens/HomeScreen';
+import NeighborhoodScreen from './src/screens/NeighborhoodScreen';
+import FacilityScreen from './src/screens/FacilityScreen';
+import ReasonScreen from './src/screens/ReasonScreen';
+import ThanksScreen from './src/screens/ThanksScreen';
+
+const Stack = createStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Home" screenOptions={{ headerShown: false }}>
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Neighborhood" component={NeighborhoodScreen} />
+        <Stack.Screen name="Facility" component={FacilityScreen} />
+        <Stack.Screen name="Reason" component={ReasonScreen} />
+        <Stack.Screen name="Thanks" component={ThanksScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
